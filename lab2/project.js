5a05b6faf22a50d4e16db65fa00e6d3ef8f7e91a
Kamil Konderak <kamilkonderak@hotmail.com>
2018-03-17 08:22:52 +0100

    add project

---
+++ lab2/project.js
@@ -0,0 +1,25 @@
+var ComputingServices = {}
+ComputingServices = (function(){
+
+    var name;
+    var cost;
+    var availability;
+
+    function Service (name, cost, availability)
+
+    var MachineLearning =  new Service ("test", 120, true);
+    
+
+
+    return  {
+        message: priv2 + " World",
+        counter: function() {
+            return ++c;
+        },
+        sayHello: function(name){
+            return "Hello " + name;
+        }
+    };
+})();
+
+console.log()
