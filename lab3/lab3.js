5bd6cebfae8c52715528511bdf04fc3e558df238
Kamil Konderak <kamilkonderak@hotmail.com>
2018-03-17 10:42:32 +0100

    end of lab

--- lab3/lab3.js
+++ lab3/lab3.js
@@ -45,3 +45,18 @@ aShape.getX();
 aShape.move(3,3);
 console.log(aShape.getX());
 console.log(aShape.getY());
+
+function Circle (x,y,r) {
+    Shape.call(this, x,y)
+    this.r = r;
+}
+
+Circle.prototype = Object.create(Shape.prototype);
+Circle.prototype.constructor = Circle;
+Circle.prototype.area = function() {
+    return Math.PI * this.r * this.r;
+};
+
+var aCircle = new Circle(20,30,2);
+
+console.log(aCircle.area());
