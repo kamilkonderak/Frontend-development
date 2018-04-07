7e685330195302d03bfaf11526ac0911373ce30e
Kamil Konderak <kamilkonderak@hotmail.com>
2018-04-07 10:39:53 +0200

    app start

---
+++ lab5/app.js
@@ -0,0 +1,30 @@
+var q = $('#q');
+var keyups = Rx.Observable.fromEvent(q, 'keyup');
+
+
+keyups.throttletime(500)
+  .map(function(x){
+    return q.val();
+  })
+  .switchMap(function(x){
+    return searchWikiPedia(x);
+  })
+  .do(function(x){
+    console.log(x);
+  })
+  .subscribe(function(x){
+    $('#results').text(x);
+  });
+
+  function searchWikiPedia(term){
+    return $.ajax({
+        url: 'http://en.wikipedia.org/w/api.php',
+        datatype: 'jsonp',
+        data: {
+          action: 'opensearch',
+          formant: 'json',
+          search: term
+        }
+    }).promise();
+
+  }
