8a1956394eebc531ecace9cea14119761e0d7ce9
Kamil Konderak <kamilkonderak@hotmail.com>
2018-06-15 14:38:06 +0200

    update

--- lab10/projekt-app/src/app/service-add/service-add.component.ts
+++ lab10/projekt-app/src/app/service-add/service-add.component.ts
@@ -1,7 +1,9 @@
 import { ServiceService } from "../service.service"
+import { Service} from "../service/service"
 import { Component, OnInit }                  from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { forbiddenNameValidator } from '../shared/forbidden-name';
+
 
 @Component({
   selector: 'app-service-add',
@@ -10,9 +12,12 @@ import { forbiddenNameValidator } from '../shared/forbidden-name';
 })
 export class ServiceAddComponent implements OnInit {
 
+  services: Service[];
+  serviceService = ServiceService;
+  
   availability = ['Yes', 'No'];
 
-  service = {name: 'Dr.', price: '', isavailable: this.availability[0]};
+  service = {name: '', price: '', amount: '', isavailable: this.availability[0]};
 
   serviceForm: FormGroup;
 
@@ -23,12 +28,27 @@ export class ServiceAddComponent implements OnInit {
         Validators.minLength(6),
         forbiddenNameValidator(/bob/i) // <-- Here's how you pass in the custom validator.
       ]),
-      'price': new FormControl(this.service.price),
-      'isavailable': new FormControl(this.service.isavailable, Validators.required)
+      'price': new FormControl(this.service.price, Validators.min(0)),
+      'isavailable': new FormControl(this.service.isavailable, Validators.required ),
+      'amount': new FormControl(this.service.amount, Validators.min(0))
+    //  'amount': new FormControl(this.service.amount, Validators.min(0))
     });
   }
 
   get name() { return this.serviceForm.get('name'); }
-
+  get price() {return this.serviceForm.get('price');}
+  get amount() {return this.serviceForm.get('amount');}
   get isavailable() { return this.serviceForm.get('isavailable'); }
+/* 
+  add(name: string): void {
+    name = name.trim();
+    if (!name) { return; }
+    this.serviceService;
+    this.serviceService.addService({ name } as Service)
+      .subscribe(service => {
+        this.services.push(service);
+      });
+  } */
+
+
 }
